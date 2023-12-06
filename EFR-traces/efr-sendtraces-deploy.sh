d4cdeafca886346b3aa93d5350ede59a8bcde516
timothe.rosaz <timothe.rosaz@middleware-solutions.fr>
2023-12-06 12:11:29 +0100

    Upgraded EFR-Traces with Quarkus 2.1

--- EFR-traces/efr-sendtraces-deploy.sh
+++ EFR-traces/efr-sendtraces-deploy.sh
@@ -5,18 +5,18 @@ MEDIATION=efr-sendtraces
 echo "Mediation $MEDIATION version $VERSION."
 
 kamel run \
---name $MEDIATION \
-routes/servicesEFR.xml  \
-routes/servicesOIDC.xml  \
-routes/efr-sendtraces.xml  \
---property file:config/$ENVIRONMENT/run.properties \
---property quarkus.application.name=$MEDIATION \
--t logging.level=INFO \
---build-property file:config/build.properties \
---label mediation=$MEDIATION --label version=$VERSION \
--t toleration.enabled=true \
--t toleration.taints="kubernetes.azure.com/scalesetpriority=spot:NoSchedule" \
---wait
+    --name $MEDIATION \
+    routes/servicesEFR.xml \
+    routes/servicesOIDC.xml \
+    routes/efr-sendtraces.xml \
+    --property file:config/$ENVIRONMENT/run.properties \
+    --property quarkus.application.name=$MEDIATION \
+    -t logging.level=INFO \
+    --build-property file:config/build.properties \
+    --label mediation=$MEDIATION --label version=$VERSION \
+    -t toleration.enabled=true \
+    -t toleration.taints="kubernetes.azure.com/scalesetpriority=spot:NoSchedule" \
+    --wait
 
 kubectl delete hpa/$MEDIATION
 kubectl autoscale it $MEDIATION --min=1 --max=3 --cpu-percent=80
